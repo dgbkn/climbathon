0c79af4f6197b69e30af025502c6fd9f618034b5
dgbkn <anandrambkn@gmail.com>
2023-11-08 21:04:31 +0530

    more advanced backend checking

--- worker.js
+++ worker.js
@@ -1,104 +1,130 @@
-addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event.request));
-});
-
-const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "HEAD,POST,OPTIONS",
-  "Access-Control-Max-Age": "86400",
-  "Access-Control-Allow-Headers": "Content-Type",
-};
-
-function standardizePhoneNumber(phoneNumber) {
-  phoneNumber = phoneNumber.replace(/\+|91/g, "");
-  return phoneNumber;
-}
-
-function isValidIndianPhoneNumber(phoneNumber) {
-  const regex = /^(?:\+91|91)?[0-9]{10}$/;
-  return regex.test(phoneNumber);
-}
-
-async function handleRequest(request) {
-  try {
-    // Check if the request method is POST
-    if (request.method === "POST") {
-      const formData = await request.formData();
-
-      var name = formData.get("name");
-      var email = formData.get("email");
-      var age = formData.get("age");
-      var contact = formData.get("phone");
-      var clgName = formData.get("clgName");
-      var schoolName = formData.get("sclName");
-      var rNo = formData.get("rno");
-      var studentIdUrl = formData.get("studentIdUrl") ?? ""; //image
-      var govIdUrl = formData.get("govIdUrl") ?? ""; //image
-      var ssPaymentUrl = formData.get("ssPayment") ?? ""; // image
-      var tId = formData.get("trnID");
-      var paymentPhone = formData.get("trnPhone");
-      var gender = formData.get("gender");
-      var studentCategory = formData.get("category");
-      var paymentGateway = formData.get("gateway");
-      var runningDistance = formData.get("distance");
-
-      await submitToGoogleForm(
-        name,
-        email,
-        age,
-        contact,
-        clgName,
-        schoolName,
-        studentIdUrl,
-        govIdUrl,
-        rNo,
-        ssPaymentUrl,
-        tId,
-        paymentPhone,
-        gender,
-        studentCategory,
-        paymentGateway,
-        runningDistance
-      );
-
-      const successResponse = {
-        success: "Form data received and submitted successfully",
-      };
-      return new Response(JSON.stringify(successResponse), {
-        status: 200,
-        headers: {
-          "Content-Type": "application/json",
-          ...corsHeaders,
-        },
-      });
-    } else {
-      // Handle other HTTP methods
+addEventListener('fetch', (event) => {
+    event.respondWith(handleRequest(event.request));
+  });
+  
+  const corsHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'HEAD, POST, OPTIONS',
+    'Access-Control-Max-Age': '86400',
+    'Access-Control-Allow-Headers': 'Content-Type',
+  };
+  
+  function standardizePhoneNumber(phoneNumber) {
+    phoneNumber = phoneNumber.replace(/\+|91/g, '');
+    return phoneNumber;
+  }
+  
+  function isValidIndianPhoneNumber(phoneNumber) {
+    const regex = /^(?:\+91|91)?[0-9]{10}$/;
+    return regex.test(phoneNumber);
+  }
+  
+  async function handleRequest(request) {
+    try {
+      // Check if the request method is POST
+      if (request.method === 'POST') {
+        const formData = await request.formData();
+  
+        // Verify required fields
+        const requiredFields = ['name', 'email', 'age', 'phone', 'trnID', 'trnPhone', 'gender', 'category', 'gateway', 'distance'];
+        for (const field of requiredFields) {
+          if (!formData.has(field)) {
+            const errorResponse = {
+              error: `Missing required field: ${field}`,
+            };
+            return new Response(JSON.stringify(errorResponse), {
+              status: 400, // Bad Request
+              headers: {
+                'Content-Type': 'application/json',
+                ...corsHeaders,
+              },
+            });
+          }
+        }
+  
+        // Optional fields
+        const clgName = formData.get('clgName');
+        const schoolName = formData.get('sclName');
+        const rNo = formData.get('rno');
+        const studentIdUrl = formData.get('studentIdUrl') || ''; // image
+        const govIdUrl = formData.get('govIdUrl') || ''; // image
+        const ssPaymentUrl = formData.get('ssPayment') || ''; // image
+  
+        // Validate phone number
+        const contact = standardizePhoneNumber(formData.get('phone'));
+        if (!isValidIndianPhoneNumber(contact)) {
+          const errorResponse = {
+            error: 'Invalid phone number',
+          };
+          return new Response(JSON.stringify(errorResponse), {
+            status: 400, // Bad Request
+            headers: {
+              'Content-Type': 'application/json',
+              ...corsHeaders,
+            },
+          });
+        }
+  
+        // Call the submitToGoogleForm function with the verified and formatted data
+        await submitToGoogleForm(
+          formData.get('name'),
+          formData.get('email'),
+          formData.get('age'),
+          contact,
+          clgName,
+          schoolName,
+          studentIdUrl,
+          govIdUrl,
+          rNo,
+          ssPaymentUrl,
+          formData.get('trnID'),
+          formData.get('trnPhone'),
+          formData.get('gender'),
+          formData.get('category'),
+          formData.get('gateway'),
+          formData.get('distance')
+        );
+  
+        const successResponse = {
+          success: 'Form data received and submitted successfully',
+        };
+        return new Response(JSON.stringify(successResponse), {
+          status: 200, // OK
+          headers: {
+            'Content-Type': 'application/json',
+            ...corsHeaders,
+          },
+        });
+      } else {
+        // Handle other HTTP methods
+        const errorResponse = {
+          error: 'Only POST requests are supported',
+        };
+        return new Response(JSON.stringify(errorResponse), {
+          status: 405, // Method Not Allowed
+          headers: {
+            'Content-Type': 'application/json',
+            ...corsHeaders,
+          },
+        });
+      }
+    } catch (error) {
+      // Handle errors
       const errorResponse = {
-        error: "Only POST requests are supported",
+        error: `Error: ${error.message}`,
       };
       return new Response(JSON.stringify(errorResponse), {
-        status: 200,
+        status: 500, // Internal Server Error
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
           ...corsHeaders,
         },
       });
     }
-  } catch (error) {
-    // Handle errors
-    const errorResponse = {
-      error: `Error: ${error.message}`,
-    };
-    return new Response(JSON.stringify(errorResponse), {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json",
-        ...corsHeaders,
-      },
-    });
   }
-}
 
+  
+  
 async function submitToGoogleForm(
   name,
   email,
